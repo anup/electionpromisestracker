3effbe1ac3d32a7f79f3792acf6b951fd42921fd
Anup Kalbalia <anup.kalbalia@gmail.com>
2017-02-15 23:54:08 +0530

    Made changes suitable to the education manifesto of the delhi government

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@
 
     // Dates
     var today = moment();
-    var inauguration = moment('2017-01-20');
+    var inauguration = moment('2015-02-14');
     $('#inauguration-days').html(inauguration.diff(today, 'days') > 0 ? inauguration.diff(today, 'days') : 'NA');
     
     $('#inauguration-time-container').hide(); //TEMP FIX 
